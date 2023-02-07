0a721a1e718ce56e697319b79bc2b18bac1bdc7a
Oliwia <michalina.rybacka.00@gmail.com>
2023-02-07 16:46:00 +0100

    Dodaj pliki projektów.

---
+++ kasyno/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace kasyno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = 0.75;
+            guy player = new guy() { Cash = 100, Name = "Gracz" };
+            
+
+            Console.WriteLine("Witaj w kasynie. Prawdopodobieństwo przegranej : 0,75");
+            player.WriteMyInfo();
+            while (player.Cash > 0)
+            {
+
+                double value = random.NextDouble();
+                Console.WriteLine("Proszę podać stawianą kwotę : ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = amount * 2;
+                    //Console.WriteLine(pot);
+                    if (value > odds)
+                    {
+                        Console.WriteLine("Gratuluję, wygrałeś !");
+                        player.ReceiveCash(pot);
+                        player.WriteMyInfo();
+                    }
+                    else if (value < odds)
+                    {
+                        Console.WriteLine("Niestety przegrałeś, spróbuj jeszcze raz !");
+                        player.Cash -= amount;
+                        player.WriteMyInfo();
+
+
+                    }
+                }
+            
+
+
+            }
+
+        }
+    }
+}
---
+++ kasyno/guy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kasyno
+{
+    internal class guy
+    {
+        public string Name;
+        public int Cash;
+
+        /// <summary>
+        /// metoda opisuje ile hajsu ma jaki ziomek
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name +" ma " + Cash + " zł.");
+        }
+        /// <summary>
+        /// przekazuje hajs i usuwa go z portfela V pokazuje info o braku środków
+        /// </summary>
+        /// <param name="amount"> Przekazywana kwota do typka</param>
+        /// <returns>Ilość pieniędzy wyjmowanych z portfela lub 0, jeśli dostępnę środki są za małe lub parametr amount jest nieprawidłowy</returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " mówi: " + amount + " nie jest poprawną kwotą.");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + " mówi: " + " nie mam wystarczających środków, aby dać ci " + amount + " zł.");
+                return 0;
+            }
+            
+            Cash -= amount;
+            return amount;
+        }
+        public void ReceiveCash(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine(Name +" mówi: "+  "nie przyjmę " + amount + " zł.");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+    }
+}
